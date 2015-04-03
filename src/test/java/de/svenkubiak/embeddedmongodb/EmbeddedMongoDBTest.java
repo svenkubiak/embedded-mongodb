5cd96bb451a794705caf6864cef54fb7358c5dbc
Sven Kubiak <sk@svenkubiak.de>
2015-04-03 15:01:20 +0200

    updated unit tests to mongodb 3.0

--- src/test/java/de/svenkubiak/embeddedmongodb/EmbeddedMongoDBTest.java
+++ src/test/java/de/svenkubiak/embeddedmongodb/EmbeddedMongoDBTest.java
@@ -3,12 +3,12 @@ package de.svenkubiak.embeddedmongodb;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.bson.Document;
 import org.junit.Test;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 /**
  * 
@@ -24,14 +24,14 @@ public class EmbeddedMongoDBTest {
 	    MongoClient mongoClient = EmbeddedMongo.DB.getMongoClient();
 		assertNotNull(mongoClient);
 		
-		DB db = mongoClient.getDB("embeddedTestDB");
+		MongoDatabase db = mongoClient.getDatabase("embeddedTestDB"); 
 		assertNotNull(db);
 		
-		DBCollection collection = db.getCollection("testCollection");
+		MongoCollection<Document> collection = db.getCollection("testCollection");
 		assertNotNull(collection);
 		
 		for (int i=0; i < 100; i++) {
-			collection.insert(new BasicDBObject("i", i));
+			collection.insertOne(new Document("i", i));
 		}
 		
 		assertEquals(100, collection.count());
