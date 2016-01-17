ed5785e96c83fe52392862f14747ed0c63fc902a
Sven Kubiak <sk@svenkubiak.de>
2016-01-17 16:06:11 +0100

    version changed to v3_2 from v3_0

--- src/main/java/de/svenkubiak/embeddedmongodb/EmbeddedMongo.java
+++ src/main/java/de/svenkubiak/embeddedmongodb/EmbeddedMongo.java
@@ -28,55 +28,55 @@ public enum EmbeddedMongo {
     private boolean mongoIPv6;
     private String mongoHost = "localhost";
     private int mongoPort;
-    
+
     private EmbeddedMongo() {
         try {
             this.mongoPort = SecureRandom.getInstance(ALGORITHM).nextInt(MAX_PORT) + MIN_PORT;
-        } catch (NoSuchAlgorithmException e) {
+        } catch (final NoSuchAlgorithmException e) {
             LoggerFactory.getLogger(EmbeddedMongo.class).error("Failed to init EmbeddedMongo", e);
         }
     }
-    
+
     public EmbeddedMongo port(int port) {
         this.mongoPort = port;
         return this;
     }
-    
+
     public EmbeddedMongo host(String host) {
         this.mongoHost = host;
         return this;
     }
-    
+
     public EmbeddedMongo ipv6(boolean ipv6) {
         this.mongoIPv6 = ipv6;
         return this;
     }
-    
+
     public void start() {
         if (!this.active) {
             try {
                 this.mongodProcess = MongodStarter.getDefaultInstance().prepare(new MongodConfigBuilder()
-                .version(Version.Main.V3_0)
+                .version(Version.Main.V3_2)
                 .net(new Net(this.mongoHost, this.mongoPort, this.mongoIPv6))
                 .build()).start();
-                
+
                 LoggerFactory.getLogger(EmbeddedMongo.class).info("Successfully started EmbeddedMongoDB @ {}:{}", this.mongoHost, this.mongoPort);
                 this.active = true;
-            } catch (IOException e) {
+            } catch (final IOException e) {
                 LoggerFactory.getLogger(EmbeddedMongo.class).error("Failed to start EmbeddedMongoDB", e);
-            }  
+            }
         }
     }
-    
+
     public void stop() {
         if (this.active) {
-            this.mongodProcess.stop();   
+            this.mongodProcess.stop();
             this.active = false;
-            
+
             LoggerFactory.getLogger(EmbeddedMongo.class).info("Successfully stopped EmbeddedMongoDB @ {}:{}", this.mongoHost, this.mongoPort);
         }
     }
-    
+
     public MongoClient getMongoClient() {
         return new MongoClient(this.getHost(), this.getPort());
     }
@@ -84,11 +84,11 @@ public enum EmbeddedMongo {
     public int getPort() {
         return this.mongoPort;
     }
-    
+
     public String getHost() {
         return this.mongoHost;
     }
-    
+
     public boolean isIPv6() {
         return this.mongoIPv6;
     }
