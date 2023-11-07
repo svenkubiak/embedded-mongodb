565f593059af94a71547e5f56319d6f92d22304b
Sven Kubiak <sk@svenkubiak.de>
2023-11-07 10:16:17 +0100

    Updated release script

--- release.sh
+++ release.sh
@@ -9,10 +9,7 @@ else
   if [ $STATUS -ne 0 ]; then
     echo "Failed to release!"  
   else
-    git tag $STATUS
     mvn release:update-versions
-    git commit -am "Updated version after release"
-    git push origin main
   fi
 fi
 
